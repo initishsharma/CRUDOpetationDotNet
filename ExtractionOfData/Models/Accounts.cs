410c6e1bfc5d0fb090073b2db493bbe229931b2e
Nitish Sharma <nitish.sharma@cybergindia.local>
2021-04-21 16:03:12 +0530

    Basic Crud operation

---
+++ ExtractionOfData/Models/Accounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtractionOfData.Models
+{
+    public partial class Accounts
+    {
+        public Accounts()
+        {
+            Users = new HashSet<Users>();
+        }
+
+        public int AccountId { get; set; }
+        public string AccountName { get; set; }
+        public string City { get; set; }
+
+        public ICollection<Users> Users { get; set; }
+    }
+}
---
+++ ExtractionOfData/Models/BanksContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ExtractionOfData.Models
+{
+    public partial class BanksContext : DbContext
+    {
+        public BanksContext()
+        {
+        }
+
+        public BanksContext(DbContextOptions<BanksContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Accounts> Accounts { get; set; }
+        public virtual DbSet<StudentInfo> StudentInfo { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=CYG224;Database=Banks;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Accounts>(entity =>
+            {
+                entity.HasKey(e => e.AccountId);
+
+                entity.Property(e => e.AccountId).ValueGeneratedNever();
+
+                entity.Property(e => e.AccountName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<StudentInfo>(entity =>
+            {
+                entity.Property(e => e.Address)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+
+                entity.Property(e => e.UserId).ValueGeneratedNever();
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Lastname)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Account)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.AccountId)
+                    .HasConstraintName("FK__Users__AccountId__276EDEB3");
+            });
+        }
+    }
+}
